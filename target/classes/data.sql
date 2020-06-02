d53ef060d31ccb6b7d3fdd1d1bb25d66f1087282
Zuzanna Kuzniar <zzuz1015@gmail.com>
2020-06-02 20:52:03 +0200

    improved SecurityConfig and added UserDetailsService impl

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into users (id, fullname, password, username) values (1, "Zuzanna", "user123", "user")
+insert into users (id, fullname, password, username) values (2, "Admin", "admin123", "admin")
